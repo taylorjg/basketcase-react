35356fbbd807266348175d0b4463952b5fa37a08
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-06-22 00:58:40 +0100

    Add image assets and first stab at rendering a product

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,7 +1,21 @@
 import PropTypes from "prop-types";
 
 export const Product = ({ product }) => {
-  return <pre>{JSON.stringify(product, null, 2)}</pre>;
+  const pos = product.Image.lastIndexOf("/");
+  const imageName = product.Image.substring(pos + 1);
+  const productImageUrl = `assets/product-images/${imageName}`;
+
+  const brandName = product.Brand.toLowerCase();
+  const brandImageUrl = `assets/brand-images/${brandName}.png`;
+
+  return (
+    <div>
+      <img src={productImageUrl} />
+      <img src={brandImageUrl} />
+      <div>&pound;{product.Price}</div>
+      <div>{product.FullTitle}</div>
+    </div>
+  );
 };
 
 Product.propTypes = {
