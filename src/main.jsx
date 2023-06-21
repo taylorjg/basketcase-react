04d10eecb381642000041d352a767810ce14886f
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-06-22 00:19:33 +0100

    Remove guff

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { App } from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
