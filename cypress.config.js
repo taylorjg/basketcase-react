1d94ee9b2e8541a85c5e67383fd0b6c67cb6c2f0
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-06-24 09:39:11 +0100

    Use npm run dev instead of npx serve

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5174/basketcase-react",
+    baseUrl: "http://localhost:5173/basketcase-react",
     setupNodeEvents(/* on, config */) {
       // implement node event listeners here
     },
