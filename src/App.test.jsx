d0c3c1be155f5e334f4046175501b4630d82b150
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-07-06 21:28:33 +0100

    Add a unit test re url state => ui state

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,20 @@
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { ToastProvider } from "@app/hooks/toast-provider";
 import { App } from "./App";
 
-const renderApp = () => {
+const renderApp = (path) => {
   const queryClient = new QueryClient();
+  const routes = [{ path: "*", element: <App /> }];
+  const maybeOpts = path ? { initialEntries: [path] } : undefined;
+  const router = createMemoryRouter(routes, maybeOpts);
 
   return render(
     <QueryClientProvider client={queryClient}>
       <ToastProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </ToastProvider>
     </QueryClientProvider>
   );
@@ -25,6 +26,17 @@ describe("App test suite", () => {
     const productDescriptions = [
       "Beko WM5122S 5Kg Washing Machine with 1200 rpm - Silver", // product 1
       "Hotpoint WMAO743P 7Kg Washing Machine with 1400 rpm - White", // product 10
+    ];
+    for (const productDescription of productDescriptions) {
+      expect(await screen.findByText(productDescription)).toBeInTheDocument();
+    }
+  });
+
+  it("URL state is reflected in the UI state", async () => {
+    renderApp("/?sortBy=1");
+    const productDescriptions = [
+      "LG TrueSteam™ F14U1JBS8 10Kg Washing Machine with 1400 rpm - Black", // product 1
+      "Hotpoint Ultima S-Line RPD10667DD 10Kg Washing Machine with 1600 rpm - White", // product 10
     ];
     for (const productDescription of productDescriptions) {
       expect(await screen.findByText(productDescription)).toBeInTheDocument();
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,16 +1,40 @@
 import { rest } from "msw";
 
 import all from "./fixtures/all.json";
+import sortBy0 from "./fixtures/sortBy0.json";
+import sortBy1 from "./fixtures/sortBy1.json";
 
-const mockSearchHandler = (_req, res, ctx) => {
-  const allPage1 = {
+const applyPagination = (all, pageSize, currentPage) => {
+  const zeroBasedPageNumber = Math.max(currentPage - 1, 0);
+  const start = pageSize * zeroBasedPageNumber;
+  const end = start + pageSize;
+  return {
     ...all,
     results: {
       ...all.results,
-      products: all.results.products.slice(0, 10),
+      products: all.results.products.slice(start, end),
     },
   };
-  return res(ctx.status(200), ctx.json(allPage1));
+};
+
+const applySortBy = (sortBy) => {
+  switch (sortBy) {
+    case 0:
+      return sortBy0;
+    case 1:
+      return sortBy1;
+    default:
+      return all;
+  }
+};
+
+const mockSearchHandler = async (req, res, ctx) => {
+  const params = await req.json();
+  const pageSize = params.pageSize ?? 10;
+  const currentPage = params.currentPage ?? 1;
+  const sortBy = params.sortBy ?? 0;
+  const results = applyPagination(applySortBy(sortBy), pageSize, currentPage);
+  return res(ctx.status(200), ctx.json(results));
 };
 
 export const handlers = [rest.post(/\/api\/search$/, mockSearchHandler)];
