a0b61bdd884c44faf3ede9726f25e44f2d5cd315
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-07-15 00:27:25 +0100

    Make facets operational (5)

--- src/components/FacetValue.jsx
+++ src/components/FacetValue.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Checkbox, FormControlLabel, Typography } from "@mui/material";
+import { Checkbox, FormControlLabel, Radio, Typography } from "@mui/material";
 
 import { StyledFacetValue } from "./FacetValue.styles";
 
@@ -10,10 +10,18 @@ export const FacetValue = ({ facet, facetValue, onToggleFacetValue }) => {
 
   const label = `${facetValue.displayName} (${facetValue.count})`;
 
+  const isSingle = facet.type === "single";
+
   return (
     <StyledFacetValue>
       <FormControlLabel
-        control={<Checkbox size="small" checked={facetValue.selected} onClick={onClick} />}
+        control={
+          isSingle ? (
+            <Radio size="small" checked={facetValue.selected} onClick={onClick} />
+          ) : (
+            <Checkbox size="small" checked={facetValue.selected} onClick={onClick} />
+          )
+        }
         label={<Typography variant="subtitle2" dangerouslySetInnerHTML={{ __html: label }} />}
         labelPlacement="end"
       />
