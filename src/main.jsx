37003b6fe98c4594ade680a55d242dfe494894a4
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-07-27 11:19:27 +0100

    Add Google Analytics (basic tag) - but not for localhost

--- src/main.jsx
+++ src/main.jsx
@@ -36,3 +36,19 @@ ReactDOM.createRoot(root).render(
   </QueryClientProvider>
   // </React.StrictMode>
 );
+
+if (window.location.hostname !== "localhost") {
+  const script1 = document.createElement("script");
+  script1.async = true;
+  script1.src = "https://www.googletagmanager.com/gtag/js?id=G-VRT62SH6ZY";
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement("script");
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-VRT62SH6ZY');
+  `;
+  document.head.appendChild(script2);
+}
