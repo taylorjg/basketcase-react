dab83cd91e5606ac778b717d82b0b269b91c791f
Jonathan Taylor <jonathan.taylor@disney.com>
2026-07-04 08:41:51 +0100

    Dependency updates (11)

--- src/setupFiles.js
+++ src/setupFiles.js
@@ -1,11 +1,8 @@
-import { expect, afterEach } from "vitest";
+import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import matchers from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom/vitest";
 
 import { server } from "@app/mocks/server.js";
-
-// extends Vitest's expect method with methods from react-testing-library
-expect.extend(matchers);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
